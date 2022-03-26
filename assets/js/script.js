6798a694483570ab5ea3af559168df47363ad95e
Natalie Bulger <bulgern20@gmail.com>
2022-03-26 14:29:03 -0700

    Commenting HTML and JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,12 +11,17 @@ $(".time").each(function(i) {
 });
 
 //Change the color of each timeblock based on calculated past present and future using moment
+
+//Create a hourNow variable
 var hourNow = moment().hours();
+//Create event handler for the task input fields
 var taskInputEl = $(".task_input");
 
+//Loop through the timedTasks array above to parse out the hour of the input field label to compare with the hourNow
 for(var i=0; i<timedTasks.length; i++){
     var substringTime = parseInt(timedTasks[i].substring(0,2));
     if(substringTime == hourNow){
+        //add the class based on the parsed time and hour now comparison
         taskInputEl[i].className += " present";
     } else if (substringTime < hourNow){
         taskInputEl[i].className += " past";
@@ -25,9 +30,9 @@ for(var i=0; i<timedTasks.length; i++){
     };
 }
 
-//save button --> local storage
-
+//event handler for the save button
 var saveBtnEl = $(".saveBtn");
+//new task array to populate tasks from input field
 var newTask = [];
 for (var i = 0; i < saveBtnEl.length; i++) {
     saveBtnEl[i].addEventListener("click", function(event){
@@ -38,18 +43,12 @@ for (var i = 0; i < saveBtnEl.length; i++) {
             newTask.splice(j, 0, taskInputEl[j].value.trim());
             localStorage.setItem("tasks", JSON.stringify(newTask));
         };
+        //Used an alert box as a confirmation to the user that the content was saved
+        alert("Saved!");
+        //Below is what I would like to do for my saved confirmation but it only worked the first time any saved button was clicked, but not after unless I reloaded the page
+        // $(".saved").text("Saved!").fadeOut(500);
     });
 };
-
-// for (var i = 0; i < saveBtnEl.length; i++) {
-//     saveBtnEl[i].addEventListener("click", function(){
-//         $(".saved").text("Saved!");
-//         var saved = function(){
-//             $(".saved").fadeOut();
-//         };
-//         setTimeout(saved, 750);
-//     });
-// };
 
 
 //take things out of local storage and populate on page load
@@ -63,7 +62,7 @@ $(window).on('load', function(){
     }
 });
 
-//add a clear button to clear out tasks
+//add a clear button to clear out all tasks at the top of the page
 $(".clear-tasks").on("click", function (){
     localStorage.clear();
     window.location.reload();
